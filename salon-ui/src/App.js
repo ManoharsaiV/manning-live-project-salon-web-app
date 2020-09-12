3d4393df81f89028dafa828e00e70ae89ad3462f
Manohar <42617348+ManoharsaiV@users.noreply.github.com>
2020-09-12 00:35:40 -0400

    Add files via upload

---
+++ salon-ui/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import {Navbar, Nav, NavItem, NavDropdown} from 'react-bootstrap';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+
+function App() {
+    return (
+        <div className="App">
+            <Navbar bg="dark" variant="dark">
+                <Navbar.Brand> AR Salon and Day Spa </Navbar.Brand>
+            </Navbar>
+        </div>
+    );
+}
+
+export default App;
